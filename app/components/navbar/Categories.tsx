ff7796892e43376c4a9847aac47fec50dbabaa48
Anten Manuuel <anten.manuuel@stonybrook.edu>
2024-01-25 21:26:28 -0500

    implement categories ui

---
+++ app/components/navbar/Categories.tsx
@@ -0,0 +1,43 @@
+"use client";
+import { usePathname, useSearchParams } from "next/navigation";
+
+import {categories} from "app/constants/categories";
+import CategoryBox from "../CategoryBox";
+import Container from "../Container";
+
+const Categories = () => {
+  const parms = useSearchParams();
+  const category = parms?.get("category");
+  const pathname = usePathname();
+  const isMainPage = pathname === "/";
+
+  if (!isMainPage) {
+    return null;
+  }
+
+  return (
+    <Container>
+      <div
+        className="
+            pt-4
+            flex 
+            flex-row 
+            items-center 
+            justify-between
+            overflow-x-auto
+          "
+      >
+        {categories.map((item: any) => (
+          <CategoryBox
+            key={item.label}
+            label={item.label}
+            icon={item.icon}
+            selected={category === item.label}
+          />
+        ))}
+      </div>
+    </Container>
+  );
+};
+
+export default Categories;
--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,5 +1,6 @@
 
 import Container from "../Container";
+import Categories from "./Categories";
 import Logo from "./Logo";
 import Search from "./Search";
 import UserMenu from "./UserMenu";
@@ -37,6 +38,7 @@ const Navbar: React.FC<NavbarProps> = ({
         </div>
       </Container>
     </div>
+    <Categories />
   </div>
   );
 }
