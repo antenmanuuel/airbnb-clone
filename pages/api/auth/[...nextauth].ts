105c0f317131e91ed314771753fde38a7ef9c1bf
Anten Manuuel <anten.manuuel@stonybrook.edu>
2024-01-28 13:33:25 -0500

    add CI/CD and update env variables

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -11,8 +11,8 @@ export const authOptions: AuthOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
     GithubProvider({
-      clientId: process.env.GITHUB_ID as string,
-      clientSecret: process.env.GITHUB_SECRET as string,
+      clientId: process.env.GIT as string,
+      clientSecret: process.env.GIT_SECRET as string,
     }),
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
