d53f086904a29d26786a709126dead3f17db3a61
Anten Manuuel <anten.manuuel@stonybrook.edu>
2024-02-04 20:22:09 -0500

    implement backend logic for reservation

--- app/listings/[listingId]/page.tsx
+++ app/listings/[listingId]/page.tsx
@@ -1,7 +1,10 @@
+
 import getCurrentUser from "@/app/actions/getCurrentUser";
 import getListingById from "@/app/actions/getListingById";
+import getReservations from "@/app/actions/getReservations";
 import ClientOnly from "@/app/components/ClientOnly";
 import EmptyState from "@/app/components/EmptyState";
+
 import ListingClient from "./ListingClient";
 
 interface IParams {
@@ -9,8 +12,11 @@ interface IParams {
 }
 
 const ListingPage = async ({ params }: { params: IParams }) => {
+
   const listing = await getListingById(params);
+  const reservations = await getReservations(params);
   const currentUser = await getCurrentUser();
+
   if (!listing) {
     return (
       <ClientOnly>
@@ -18,11 +24,16 @@ const ListingPage = async ({ params }: { params: IParams }) => {
       </ClientOnly>
     );
   }
+
   return (
     <ClientOnly>
-      <ListingClient listing={listing} currentUser={currentUser} />
+      <ListingClient
+        listing={listing}
+        reservations={reservations}
+        currentUser={currentUser}
+      />
     </ClientOnly>
   );
-};
-
+}
+ 
 export default ListingPage;
